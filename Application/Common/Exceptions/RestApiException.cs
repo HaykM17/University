50e8e918257567be04b22ec5d8b3f8868bc38792
HaykM17 <dr.hayk17@gmail.com>
2025-09-30 00:31:51 +0400

    Add Exception Handler

---
+++ Application/Common/Exceptions/RestApiException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Application.Common.Exceptions;
+
+public class RestApiException : Exception
+{
+    public HttpStatusCode StatusCode { get; private set; }
+
+    public RestApiException(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
